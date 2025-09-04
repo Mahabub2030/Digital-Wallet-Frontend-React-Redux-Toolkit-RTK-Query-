6802c83055121da17a68e578e813b028f3391ba1
Mahaub Alam <mahabubalom72091@gmail.com>
2025-09-04 11:33:03 +0300

    Added some code

--- src/components/my-footer.jsx
+++ src/components/my-footer.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { t } from "i18next";
 
 const MyFooter = () => {
     return (
         <div className="my-footer text-center p-1">
             {t("footer.created_by")} Iván Jaén Trujillo -
-            <a href="https://github.com/kholostyak17" className="text-decoration-none primary-color" target="_blank" rel="noreferrer"> @kholostyak17</a>
+            <a href="https://github.com/mahabub2030" className="text-decoration-none primary-color" target="_blank" rel="noreferrer"> Mahabub ALam</a>
         </div>
     );
 };
